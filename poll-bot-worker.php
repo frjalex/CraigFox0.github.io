ff91f6df8aa7afa6d1fee514cb698e8ad781a1a0
CraigFox0 <cefox2001@gmail.com>
2016-02-07 20:14:51 -0500

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '187719420:AAGFC_Fbt5ysYp1CRZSPTVd9YD-QsboAEkY');
 
-$bot = new PollBot(BOT_TOKEN, 'PollBotChat');
+$bot = new PollBot(BOT_TOKEN, 'Nomicsbot');
 $bot->runLongpoll();
